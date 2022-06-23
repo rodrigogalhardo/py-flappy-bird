f93b1587175a43ef68423a13f505a1069d36fc59
Galhardo <galhardo-m1@Galhardos-Mac-mini.local>
2022-06-23 19:03:12 -0300

    Ajustes do Jogo para inicar com ação do Usuário

--- flappy.py
+++ flappy.py
@@ -15,6 +15,10 @@ PIPE_HEIGHT = 500
 
 PIPE_GAP = 200
 
+START = False
+
+
+# Criando o Pássaro
 class Bird(pygame.sprite.Sprite):
 
     def __init__(self):
@@ -37,23 +41,25 @@ class Bird(pygame.sprite.Sprite):
 
     def update(self):
         self.current_image = (self.current_image + 1) % 3
-        self.image = self.images[ self.current_image ]
+        self.image = self.images[self.current_image]
 
         self.speed += GRAVITY
 
         # Update height
         self.rect[1] += self.speed
-    
+
     def bump(self):
         self.speed = -SPEED
 
+
+# Criando os Canos
 class Pipe(pygame.sprite.Sprite):
 
     def __init__(self, inverted, xpos, ysize):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load('pipe-red.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (PIPE_WIDTH,PIPE_HEIGHT))
+        self.image = pygame.transform.scale(self.image, (PIPE_WIDTH, PIPE_HEIGHT))
 
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
@@ -69,6 +75,8 @@ class Pipe(pygame.sprite.Sprite):
     def update(self):
         self.rect[0] -= GAME_SPEED
 
+
+# Criando o Chão/Superficie
 class Ground(pygame.sprite.Sprite):
 
     def __init__(self, xpos):
@@ -82,56 +90,26 @@ class Ground(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
         self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
-    
+
     def update(self):
         self.rect[0] -= GAME_SPEED
 
+
+# Se cai fora da Tela
 def is_off_screen(sprite):
     return sprite.rect[0] < -(sprite.rect[2])
 
+
+# Cria dinamicamente novos Canos
 def get_random_pipes(xpos):
     size = random.randint(100, 300)
     pipe = Pipe(False, xpos, size)
     pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
-    return (pipe, pipe_inverted)
+    return pipe, pipe_inverted
 
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-BACKGROUND = pygame.image.load('background-day.png')
-BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
-bird_group = pygame.sprite.Group()
-bird = Bird()
-bird_group.add(bird)
-
-ground_group = pygame.sprite.Group()
-for i in range(2):
-    ground = Ground(GROUND_WIDTH * i)
-    ground_group.add(ground)
-
-pipe_group = pygame.sprite.Group()
-for i in range(2):
-    pipes = get_random_pipes(SCREEN_WIDTH * i + 800)
-    pipe_group.add(pipes[0])
-    pipe_group.add(pipes[1])
-
-
-clock = pygame.time.Clock()
-
-while True:
-    clock.tick(30)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                bird.bump()
-
-    screen.blit(BACKGROUND, (0, 0))
-
+# Inicia o Jogo
+def start_game():
     if is_off_screen(ground_group.sprites()[0]):
         ground_group.remove(ground_group.sprites()[0])
 
@@ -158,7 +136,54 @@ while True:
     pygame.display.update()
 
     if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or
-       pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
+            pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
         # Game over
-        input()
-        break
+        # input()
+        pygame.quit()
+
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+BACKGROUND = pygame.image.load('background-day.png')
+BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+bird_group = pygame.sprite.Group()
+bird = Bird()
+bird_group.add(bird)
+
+ground_group = pygame.sprite.Group()
+for i in range(2):
+    ground = Ground(GROUND_WIDTH * i)
+    ground_group.add(ground)
+
+pipe_group = pygame.sprite.Group()
+for i in range(2):
+    pipes = get_random_pipes(SCREEN_WIDTH * i + 800)
+    pipe_group.add(pipes[0])
+    pipe_group.add(pipes[1])
+
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(30)
+
+    if START:  # Se START for True (Verdadeiro) cria um loop no jogo.
+        start_game()
+
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:  # Tecla ESC
+                pygame.quit()
+            if event.key == K_SPACE:  # Tecla Barra de Espaco
+                if not START:  # Se START == FALSE (Falso) Prepara o jogo para iniciar.
+                    START = True
+                    start_game()
+
+                bird.bump()
+            # Outro exemplo de iniciar o Jogo
+            # if event.key == K_RETURN:  # Tecla ENTER
+            #     START = True
+            #     start_game()
+
+    screen.blit(BACKGROUND, (0, 0))
